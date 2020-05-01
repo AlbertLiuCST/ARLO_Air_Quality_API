c714f6bc7d681c1e6f025c7972991d34f34fc9b6
SmallYellowFace <jctheathlete@gmail.com>
2020-05-01 12:26:06 -0700

    init branch

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify, make_response,request
 from flask_swagger_ui import get_swaggerui_blueprint
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Date, cast
-#from flask_cors import CORS, cross_origin
+from flask_cors import CORS, cross_origin
 import jwt
 import time
 import datetime
@@ -14,8 +14,9 @@ app.config['SECRET_KEY'] = 'thisissecret'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:admin@localhost/postgres'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Zh6Q6C97@database-issp-air-quality-instance.cmamvcvbojfv.us-west-2.rds.amazonaws.com/airQualityApiDb'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['CORS_HEADERS'] = 'Content-Type'
 db = SQLAlchemy(app)
-#cors = CORS(app, resources={r"/readings": {"origins": "http://localhost:3000"}})
+# cors = CORS(app, resources={r"/readings": {"origins": "http://localhost:3000"}})
 
 ### swagger specific ###
 SWAGGER_URL = '/swagger'
@@ -68,6 +69,7 @@ def token_required(f):
 #Post request by passing json payload and return specified data 
 @app.route("/readings", methods=['POST'])
 #@cross_origin(origin='localhost',headers=['Content-Type','application/json'])
+@cross_origin()
 def records_test():
     data = request.get_json()
     recordsTestData = Records_test.query.all()
@@ -123,10 +125,6 @@ def device_test():
         output.append(device_test_data)
     return jsonify({'device_test_data' : output})
 
-@app.route("/")
-def home():
-    return "<p>Hello World main page<p>"
-
 @app.route('/unprotected')
 def unprotected():
     return jsonify({'message' : 'Anyone can see this!'})
@@ -136,17 +134,26 @@ def unprotected():
 def protected():
     return jsonify({'message' : 'This only for people with valid token!'})
 
-@app.route('/login')
+@app.route('/login', methods=['POST'])
+@cross_origin()
 def login():
-    auth = request.authorization
-    if not auth or not auth.username or not auth.password:
+    #auth = request.authorization
+    auth = request.get_json()
+
+    #auth = request.form
+    resp = make_response(render_template('readcookie.html'))
+
+    if not auth or not auth['username'] or not auth['password'] :
         return make_response('Could not verify', 401, {'WWW-Authenticate' : 'Basic realm="Login required!"'})
    
-    if auth and auth.password == 'password':
-        token = jwt.encode({'user': auth.username, 'exp': datetime.datetime.utcnow()+ datetime.timedelta(minutes=30)},app.config['SECRET_KEY'])
-        return jsonify({'token' : token.decode('UTF-8')})
+    if auth and auth['password'] == 'password':
+        token = jwt.encode({'user': auth['username'], 'exp': datetime.datetime.utcnow()+ datetime.timedelta(minutes=30)},app.config['SECRET_KEY'])
+        resp = jsonify({'login': True})
+        set_access_cookies(resp, access_token)
+        # return jsonify({'token' : token.decode('UTF-8')})
+        return resp
 
-    return make_response('Could not verify', 401, {'WWW-Authenticate' : 'Basic realm="Login required!"'})
+    return make_response('Could not verify', 401)
 
 if __name__ == '__main__':
     app.run(debug=True)
