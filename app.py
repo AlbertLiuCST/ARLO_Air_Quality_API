7f8f9f4ffece5234a4e47386a966fb327ddac6d4
AlbertLiuCST <albertliu1992@hotmail.com>
2020-05-13 11:35:24 -0700

    Added Oauth code

--- app.py
+++ app.py
@@ -3,22 +3,31 @@ from flask_swagger_ui import get_swaggerui_blueprint
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Date, cast
 from flask_cors import CORS, cross_origin
+
+import json
 import jwt
 import time
 import datetime
 from datetime import date
 from functools import wraps
 from flask_session import Session
+from six.moves.urllib.request import urlopen
+from jose import jwt
 
 SESSION_TYPE = 'filesystem'
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'thisissecret'
+app.config['CLIENT_ID'] = 'xMyRktEjNm1MJz2W6KO2bTeBImCjZRjg'
+app.config['CLIENT_SECRET'] = '2rWdiq9DDhIjfz_zeswEvgTmztGxPos6T-TkMf-QhpExttiX6LqkUd5y68qJkdwD'
+app.config['TOKEN_ENDPOINT'] = 'https://arlo-aq-api.auth0.com/oauth/token'
+app.config['AUDIENCE'] = 'https://ARLO-AQ/api'
+app.config['SECRET_KEY'] = '2rWdiq9DDhIjfz_zeswEvgTmztGxPos6T-TkMf-QhpExttiX6LqkUd5y68qJkdwD'
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:admin@localhost/postgres'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Zh6Q6C97@database-issp-air-quality-instance.cmamvcvbojfv.us-west-2.rds.amazonaws.com/airQualityApiDb'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['CORS_HEADERS'] = 'Content-Type'
 db = SQLAlchemy(app)
 
+ALGORITHMS = ["RS256"]
 # cors = CORS(app, resources={r"/readings": {"origins": "http://localhost:3000"}})
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
@@ -57,18 +66,105 @@ class Records_test(db.Model):
     timestamp = db.Column(db.DateTime(timezone=True))
 
 
+class AuthError(Exception):
+    def __init__(self, error, status_code):
+        self.error = error
+        self.status_code = status_code
+
+
+@app.errorhandler(AuthError)
+def handle_auth_error(ex):
+    response = jsonify(ex.error)
+    response.status_code = ex.status_code
+    return response
+
+
+def get_token_auth_header():
+    """Obtains the access token from the Authorization Header
+    """
+    auth = request.headers.get("Authorization", None)
+    if not auth:
+        raise AuthError({"code": "authorization_header_missing",
+                        "description":
+                            "Authorization header is expected"}, 401)
+
+    parts = auth.split()
+
+    if parts[0].lower() != "bearer":
+        raise AuthError({"code": "invalid_header",
+                        "description":
+                            "Authorization header must start with"
+                            " Bearer"}, 401)
+    elif len(parts) == 1:
+        raise AuthError({"code": "invalid_header",
+                        "description": "Token not found"}, 401)
+    elif len(parts) > 2:
+        raise AuthError({"code": "invalid_header",
+                        "description":
+                            "Authorization header must be"
+                            " Bearer token"}, 401)
+
+    token = parts[1]
+    return token
+
 ##setup flask jwt token
 def token_required(f):
     @wraps(f)
     def decorated(*args, **kwargs):
-        token = request.args.get('token') #http://127.0.0.1:5000/route?token=<token key>
-        if not token:
-            return jsonify({'message' : 'Token is missing!'}), 401
+        token = get_token_auth_header()
+
+        
+        jsonurl = urlopen("https://arlo-aq-api.auth0.com/.well-known/jwks.json")
+        jwks = json.loads(jsonurl.read())
         try:
-            data = jwt.decode(token,app.config['SECRET_KEY'])
-        except:
-            return jsonify({'message' : 'Token is invalid!'}), 401
-        return f(*args, **kwargs)
+            unverified_header = jwt.get_unverified_header(token)
+        except jwt.JWTError:
+            raise AuthError({"code": "invalid_header",
+                            "description":
+                                "Invalid header. "
+                                "Use an RS256 signed JWT Access Token"}, 401)
+        if unverified_header["alg"] == "HS256":
+            raise AuthError({"code": "invalid_header",
+                            "description":
+                                "Invalid header. "
+                                "Use an RS256 signed JWT Access Token"}, 401)
+        rsa_key = {}
+        for key in jwks["keys"]:
+            if key["kid"] == unverified_header["kid"]:
+                rsa_key = {
+                    "kty": key["kty"],
+                    "kid": key["kid"],
+                    "use": key["use"],
+                    "n": key["n"],
+                    "e": key["e"]
+                }
+        if rsa_key:
+            try:
+                payload = jwt.decode(
+                    token,
+                    rsa_key,
+                    algorithms=ALGORITHMS,
+                    audience='https://ARLO-AQ/api',
+                    issuer="https://arlo-aq-api.auth0.com/"
+                )
+            except jwt.ExpiredSignatureError:
+                raise AuthError({"code": "token_expired",
+                                "description": "token is expired"}, 401)
+            except jwt.JWTClaimsError:
+                raise AuthError({"code": "invalid_claims",
+                                "description":
+                                    "incorrect claims,"
+                                    " please check the audience and issuer"}, 401)
+            except Exception:
+                raise AuthError({"code": "invalid_header",
+                                "description":
+                                    "Unable to parse authentication"
+                                    " token."}, 401)
+
+            
+            return f(*args, **kwargs)
+        raise AuthError({"code": "invalid_header",
+                        "description": "Unable to find appropriate key"}, 401)
     return decorated
 
 #Post request by passing json payload and return specified data 
